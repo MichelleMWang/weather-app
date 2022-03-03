c824f6f3efe1d21580b3a206fddd01c6acc900ee
Michelle Wang <tianw0420@gmail.com>
2022-03-03 08:07:52 -0800

    geolocation api

--- script.js
+++ script.js
@@ -7,17 +7,23 @@ const low = document.getElementById("low");
 const header = document.getElementById('header'); 
 
 function getLocation(){
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, locationError);
-      } else {
+    navigator.geolocation.getCurrentPosition(getWeather, locationError);
+    if (!navigator.geolocation) {
         header.textContent = "Geolocation is not supported by this browser.";
-      }
+    } else {
+        header.textContent = "Loading..."; 
+    }
 }
 
 function showPosition(position) {
     header.textContent = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
-  }
+}
+async function getWeather(position){
+    const info = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=c32bb3f15ac6f3b9f9a8d51f0a78543e`)
+    //const info = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=33.6481344&lon=-117.8194082&appid=c32bb3f15ac6f3b9f9a8d51f0a78543e`)
+    console.log(info); 
+}
 
 function locationError(){
     console.log('There was an error getting the location'); 
