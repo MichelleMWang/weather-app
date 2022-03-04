a761419fda68ab8a59e76f868fbd2644439fad7b
Michelle Wang <tianw0420@gmail.com>
2022-03-03 16:11:42 -0800

    got weather info the display

--- script.js
+++ script.js
@@ -21,12 +21,38 @@ function showPosition(position) {
 }
 async function getWeather(position){
     const info = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=c32bb3f15ac6f3b9f9a8d51f0a78543e`)
-    //const info = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=33.6481344&lon=-117.8194082&appid=c32bb3f15ac6f3b9f9a8d51f0a78543e`)
-    console.log(info); 
+    //const info = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=33.64&lon=-117.83&appid=c32bb3f15ac6f3b9f9a8d51f0a78543e`); 
+    const JSONED = await info.json(); 
+    header.textContent = ''; //clearing the "loading..." 
+    headerDOM(JSONED.name, JSONED.main.temp, JSONED.weather[0].description, JSONED.main.temp_min, JSONED.main.temp_max); 
 }
 
 function locationError(){
     console.log('There was an error getting the location'); 
+}
+
+function headerDOM(location, temp, desc, high, low){
+    const loc = document.createElement('div'); 
+    loc.textContent = location;
+    header.appendChild(loc); 
+
+    const temperature = document.createElement('div'); 
+    temperature.textContent = temp;
+    header.appendChild(temperature); 
+
+    const description = document.createElement('div'); 
+    description.textContent = desc;
+    header.appendChild(description); 
+
+    const highlow = document.createElement('div'); 
+    const lowed = document.createElement('span'); 
+    lowed.textContent = "L: " + low + " ";
+    highlow.appendChild(lowed); 
+    const highed = document.createElement('span'); 
+    highed.textContent = "H: " + high;
+    highlow.appendChild(highed); 
+    header.appendChild(highlow); 
+
 }
 
 getLocation(); 
